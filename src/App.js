309770d36d1e19ccaa0598cda11f42f129061300
Ali <ali.ozgur@example.com>
2023-01-17 01:24:41 +0300

    added detail button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from "react";
+import Loading from "./components/loading/Loading"
+import Info from "./components/info/Info"
+import "./App.css";
+
+
 
 function App() {
+  // önce stateleri tanımlıyorum.
+  // sayfa açılırken kullanıcıya gösterilecek mesaj
+  // const [loading, setLoading] = useState(true);
+ // API den gelen  veriyi tutacak state
+  const [show, setShow] = useState([]);
+
+  //API'den veri çekmek için fonksiyonu tanımlıyorum.
+ 
+ 
+  
+  const getImage = () => {
+    const url = `https://pixabay.com/api/?key=31725179-e9547203f59a4095ebc0c6c08&per_page=6&lang=tr`;
+
+   
+    try {
+      fetch(url)
+      .then((res) => res.json())
+      .then((data) => setShow(data.hits));
+    
+    } catch (error) {
+      console.log(error);
+     
+    }
+  }
+
+  // Api'yi render dan sonra 1 kere çağırmak için
+  //useEffect içinde kullanıyorum.
+useEffect(()=>{
+  getImage()
+},[])
+      
+ 
+console.log(show)
+
+// veri yüklenirken ekranda gösterilecek componenti çağırıyorum
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Info show={show} />
     </div>
   );
 }
--- src/components/detailInfo/DetailInfo.jsx
+++ src/components/detailInfo/DetailInfo.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 
 const DetailInfo = ({item}) => {
+  const [detail ,setDetail] = useState(false)
   const { id, tags, likes, webformatURL,type, comments} = item
  
   return (
-    <div>
+    <div className="fotograf">
       <section key={id}>
         <img src={webformatURL} alt={type} />
-        <footer>
-          <div className="picture-info">
-            <h4> Kategorisi : {tags.split(",")[2]}</h4>
-            <h4 className="picture-price">Fiyatı: 3,{likes}₺</h4>
-            <h4> Satılan Adet {comments}</h4>
-          </div>
-        </footer>
+        <button className="btn" onClick={() => setDetail(!detail)}>
+          {detail ? "Detay Gizle" : " Detayı Göster "}
+        </button>
+
+        {detail ? (
+          <footer className="display">
+            <div className="foto-info">
+              <h4> Kategorisi : {tags.split(",")[2]}</h4>
+              <h4 className="picture-price">Fiyatı: 3,{likes}₺</h4>
+              <h4> Satılan Adet {comments}</h4>
+            </div>
+          </footer>
+        ) : (
+          ""
+        )}
+        <div className="btn-sil">
+          <button className="btn-kaldır">Kaldır</button>
+        </div>
       </section>
     </div>
   );
